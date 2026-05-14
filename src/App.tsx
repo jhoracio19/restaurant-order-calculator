c39e481f01f145e155de4bc32ece5c3f22f3c537
Jose Horacio <jhoracio19@hotmail.com>
2026-05-14 01:03:25 -0600

    agregando hook personalizado

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
+import MenuItem from "./components/MenuItem";
 import { menuItems  } from "./data/db"
+import useOrder from "./hooks/useOrder";
 
 function App() {
+
+  const { addItem } = useOrder()
 
   console.log(menuItems);
   return (
@@ -10,8 +14,17 @@ function App() {
       </header>
 
       <main className="max-w-7xl mx-auto py-20 grid md:grid-cols-2">
-        <div>
-          <h2 className="">Menú</h2>
+        <div className="p-5">
+          <h2 className="text-4xl font-black">Menú</h2>
+            <div className="space-y-3 mt-10">
+                {menuItems.map(item => (
+                  <MenuItem
+                      key = {item.id}
+                      item = {item}
+                      addItem={addItem}
+                    />
+                  ))}
+            </div>
         </div>
         <div>
           <h2>Consumo</h2>
