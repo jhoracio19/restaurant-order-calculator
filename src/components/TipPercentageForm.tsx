97b732a88e3f6dcc399a889c4ce48a857dda198e
Jose Horacio <jhoracio19@hotmail.com>
2026-05-24 15:01:42 -0600

    agregando propina personalizada

--- src/components/TipPercentageForm.tsx
+++ src/components/TipPercentageForm.tsx
@@ -25,7 +25,11 @@ type TipPercentageFormProps = {
 }
 
 export default function TipPercentageForm( { setTip, tip } : TipPercentageFormProps ) {
+
+  
+
   return (
+    <>
     <div>
         <h3 className="font-black text-2xl">Propina:</h3>
 
@@ -43,7 +47,32 @@ export default function TipPercentageForm( { setTip, tip } : TipPercentageFormPr
                     />
                 </div>
             ))}
+
+            <div className="flex flex-col gap-2">
+              <label htmlFor="">Otra cantidad:</label>
+              <input
+                className="border border-gray-300 p-2 rounded-lg w-full focus:outline-none focus:ring-2 focus:ring-teal-400 focus:border-transparent transition-all"
+                type="text"
+                value={tipOptions.some(opt => opt.value === tip) ? '' : tip * 100}
+                onChange={ e => {
+                  const value = e.target.value;
+
+                  // Si esta vacio, resetea la propina a 0
+                  if(value === ''){
+                    setTip(0)
+                    return;
+                  }
+                  //  Solo convertimo a número si es número válido
+                  const numericValue = Number(value)
+                  // Se valida que sea un número y que este dentro del rango
+                  if(!isNaN(numericValue) && numericValue >= 0 && numericValue <= 100){
+                    setTip(numericValue / 100)
+                  }
+                }}
+              />
+            </div>
         </form>
     </div>
+    </>
   )
 }
